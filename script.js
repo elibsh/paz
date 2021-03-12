e3760ef4f4239311a7d66a73a4bee0be7c35c127
Eli Ben Shlush <elibsh@msn.com>
2021-03-13 00:19:35 +0200

    Finalizing First Row

---
+++ script.js
@@ -0,0 +1,7 @@
+function hoverOnReadMore(element) {
+    element.innerHTML = "<span>קרא עוד</span><img src=\"images\\read_more_blue.png\" alt=\"Read More\">";
+}
+
+function outOfReadMore(element) {
+    element.innerHTML = "<span>קרא עוד</span><img src=\"images\\read_more_white.png\" alt=\"Read More\">";
+}
